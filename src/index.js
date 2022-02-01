80993b318abcd9642414843b6bec900324a55b59
Gaffyn Voorzta <gaffyn.voorzta@hotmail.com>
2022-02-01 20:53:16 +0000

    trying to fix dotenv

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
+require('dotenv').config();
 
 ReactDOM.render(
   <React.StrictMode>
