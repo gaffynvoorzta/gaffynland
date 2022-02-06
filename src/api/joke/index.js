481bf9c03e1acc18e9bdada6fb68c8fab526e6f8
Gaffyn Voorzta <gaffyn.voorzta@hotmail.com>
2022-02-06 16:37:46 +0000

    cors...

--- src/api/joke/index.js
+++ src/api/joke/index.js
@@ -1,18 +1,18 @@
-const API_URL = `https://www.boredapi.com/api/activity`;
-const API_URL2 = `https://axoltlapi.herokuapp.com/`;
-const API_URL3 = `https://catfact.ninja/fact`;
+const API_URL = `https://calm-ravine-80567.herokuapp.com/https://www.boredapi.com/api/activity`;
+const API_URL2 = `https://calm-ravine-80567.herokuapp.com/https://axoltlapi.herokuapp.com/`;
+const API_URL3 = `https://calm-ravine-80567.herokuapp.com/https://catfact.ninja/fact`;
 
 
 export const getJoke = async () => {
-  const response = await fetch(API_URL, { mode: 'cors'});
+  const response = await fetch(API_URL);
   const json = await response.json();
   const activity = json.activity;
   //console.log(activity);
-  const response3 = await fetch(API_URL3, { mode: 'cors'});
+  const response3 = await fetch(API_URL3);
   const json3 = await response3.json();
   const joke = json3.fact;
   //console.log(json3);
-  const response2 = await fetch(API_URL2, { mode: 'cors'});
+  const response2 = await fetch(API_URL2);
   const json2 = await response2.json();
   var axoltl = json2.facts;
   if (Math.floor(Math.random() * 2) === 1) {
