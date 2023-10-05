5a075d226aba7679eabb20bfa866c17a731dca1b
Gaffyn Voorzta <78822476+gaffynvoorzta@users.noreply.github.com>
2023-10-05 16:09:47 +0100

    Update index.js

--- src/api/gaffynblog/index.js
+++ src/api/gaffynblog/index.js
@@ -1,5 +1,5 @@
 //const API_URL = `https://calm-ravine-80567.herokuapp.com/https://gaffynblog.netlify.app/api/hello`;
-const API_URL = `https://gaffynblog.netlify.app/api/hello`;
+const API_URL = `https://immense-dusk-80893-7d5edccb4ae5.herokuapp.com/https://gaffynblog.netlify.app/api/hello`;
 
 export const getGaffynBlog = async () => {
   const response = await fetch(API_URL);
