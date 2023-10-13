b0f8f91d8681c4fe5f45250aa3333797e6103613
Gaffyn Voorzta <78822476+gaffynvoorzta@users.noreply.github.com>
2023-10-13 13:32:20 +0100

    Update index.js

--- src/api/cat/index.js
+++ src/api/cat/index.js
@@ -1,5 +1,6 @@
 //const API_URL = `https://api.thecatapi.com/v1/images/search&limit=5&api_key=${process.env.REACT_APP_CAT_ACCESS_KEY}`;
-const API_URL = `https://immense-dusk-80893-7d5edccb4ae5.herokuapp.com/https://api.thecatapi.com/v1/images/search`;
+//const API_URL = `https://immense-dusk-80893-7d5edccb4ae5.herokuapp.com/https://api.thecatapi.com/v1/images/search`;
+const API_URL = `/cors-proxy/https://api.thecatapi.com/v1/images/search`;
 //const API_URL2 = `https://calm-ravine-80567.herokuapp.com/https://aws.random.cat/meow?ref=apilist.fun`
 
 export const getCat = async () => {
